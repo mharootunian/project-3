79e3ae6cfbf9d44b1bbac65281cf1203509ab5dc
Meero Harootunian <meero.harootunian@gmail.com>
2020-02-01 11:36:29 -0700

    added socket logic as well as socketio deps

--- server.js
+++ server.js
@@ -4,29 +4,35 @@ const path = require("path");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+const cors = require("cors");
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+    app.use(express.static("client/build"));
 }
 
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/facesDB";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Define API routes here
+
+
+app.use(cors());
+
+const server = app.listen(PORT, () => {
+    console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+
+const io = require('socket.io').listen(server);
+
+// io.set('origins', 'localhost:*');
+require("./lib/socketConfig")(io);
 
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
-const server = app.listen(PORT, () => {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
-
-const socketio = require("socket.io")(server);
