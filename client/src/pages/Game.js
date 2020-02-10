6d6fa74f6d7abdb37c447a2b64048a4c2f8ce179
Killoran Lehenbauer <k.lehenbauer13@gmail.com>
2020-02-10 15:25:59 -0700

    displays random image for other player to guess, added guess button with names

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 class Game extends React.Component {
     state = {
-        people: []
+        people: [],
+        yourPerson: {}
     };
 
     componentDidMount() {
@@ -16,9 +17,11 @@ class Game extends React.Component {
         let category = "Action Stars";
 
         axios.get(`/api/game/${category}`)
-        .then((res) => {
-            this.setState({ people: res.data[0].people });
-        });
+            .then((res) => {
+                let people = res.data[0].people;
+                let randomPerson = people[Math.floor(Math.random() * people.length)];
+                this.setState({ people: people, yourPerson: randomPerson });
+            });
     };
 
     render() {
@@ -26,15 +29,38 @@ class Game extends React.Component {
             <div>
                 <div className="container">
                     <div className="row">
-                        {this.state.people.map(person => (
-                            <div className="col-sm-4">
-                            <img className="img-fluid" 
-                                 key={person._id}
-                                 src={`https://image.tmdb.org/t/p/w200${person.img}`}                            />
-                                 </div>
-                        ))}
+                        <div className="col-sm-6">
+                            <div className="row">
+                                {this.state.people.map(person => (
+                                    <div className="col-sm-4">
+                                        <img className="img-fluid img-thumbnail mb-2"
+                                            key={person._id}
+                                            src={`https://image.tmdb.org/t/p/w200${person.img}`}
+                                            width="150px"
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                        <div className="col-sm-3 mx-auto text-center">
+                            <h2>Your Person:</h2>
+                            <img className="img-fluid"
+                                src={`https://image.tmdb.org/t/p/w200${this.state.yourPerson.img}`}
+                            />
+                            <div class="dropdown btn-group mt-3">
+                                <button class="btn btn-secondary dropdown-toggle" data-flip="false" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Make Guess!
+                                </button>
+                                <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+                                    {this.state.people.map(person => (
+                                        <button class="dropdown-item" type="button">{person.name}</button>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
+
             </div>
         )
     }
